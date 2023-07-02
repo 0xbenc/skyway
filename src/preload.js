87c914b66c4bb285e189073dc843a6a7407c498f
Ben Chapman <0xbenc@gmail.com>
2023-07-02 05:26:01 -0700

    IPC cleanup

--- src/preload.js
+++ src/preload.js
@@ -3,19 +3,19 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   store: {
     get(key) {
-      return ipcRenderer.sendSync('electron-store-get', key);
+      return ipcRenderer.sendSync('store-get', key);
     },
     set(property, val) {
-      ipcRenderer.send('electron-store-set', property, val);
+      ipcRenderer.send('store-set', property, val);
     }
   },
   engine: {
     chat: async (data, key) => {
-      const response = await ipcRenderer.invoke('apiRequest', "v1/chat/completions", data, key)
+      const response = await ipcRenderer.invoke('openai-api', "v1/chat/completions", data, key);
       return response;
     },
     version: async () => {
-      const response = await ipcRenderer.invoke('version')
+      const response = await ipcRenderer.invoke('version-get');
       return response;
     }
   }
