1936c14905d9a6fe5a02c40285d3a61cd777c108
Ben Chapman <0xbenc@gmail.com>
2023-09-10 19:52:06 -0700

    notifications refactor

--- src/core/notifications.js
+++ src/core/notifications.js
@@ -1,39 +1,41 @@
-import React, { useEffect } from "react";
-//
+import React, { useEffect, useMemo } from "react";
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import { useStore } from './zustand';
 
-const Notify = () => {
+const Notify = ({ notificationAlarm, notificationMessage, colorMode }) => {
   const { enqueueSnackbar } = useSnackbar();
 
-  const notification_alarm = useStore(state => state.notification_alarm);
-  const notification_message = useStore(state => state.notification_message);
-  const color_mode = useStore(state => state.color_mode);
+  const style = useMemo(() => ({
+    backgroundColor: colorMode === "light" ? "#CCCCCC" : "#333333",
+    color: colorMode === "light" ? "#111111" : "#EEEEEE",
+  }), [colorMode]);
 
   useEffect(() => {
-    if (notification_alarm) {
-      enqueueSnackbar(notification_message, {
-        style: {
-          backgroundColor: color_mode === "light" ? "#CCCCCC" : "#333333",
-          color: color_mode === "light" ? "#111111" : "#EEEEEE"
-        }
-      });
-
+    if (notificationAlarm) {
+      enqueueSnackbar(notificationMessage, { style });
       useStore.setState({ notification_alarm: false, notification_message: "" });
     }
-  }, [notification_message, notification_alarm, color_mode])
+  }, [notificationMessage, notificationAlarm, style]);
 
   return null;
 };
 
 const HandleNotifications = () => {
+  const notification_alarm = useStore(state => state.notification_alarm);
+  const notification_message = useStore(state => state.notification_message);
+  const color_mode = useStore(state => state.color_mode);
+
   return (
     <SnackbarProvider
       maxSnack={3}
       autoHideDuration={3000}
       disableWindowBlurListener={true}
     >
-      <Notify />
+      <Notify
+        notificationAlarm={notification_alarm}
+        notificationMessage={notification_message}
+        colorMode={color_mode}
+      />
     </SnackbarProvider>
   );
 };
