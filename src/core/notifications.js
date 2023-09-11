88acc3e11dbd8bd5123591fd551d4673b9369cad
Ben Chapman <0xbenc@gmail.com>
2023-09-10 17:57:21 -0700

    added style to notistack

--- src/core/notifications.js
+++ src/core/notifications.js
@@ -8,20 +8,31 @@ const Notify = () => {
 
   const notification_alarm = useStore(state => state.notification_alarm);
   const notification_message = useStore(state => state.notification_message);
+  const color_mode = useStore(state => state.color_mode);
 
   useEffect(() => {
     if (notification_alarm) {
-      enqueueSnackbar(notification_message);
-      useStore.setState({ notification_alarm: false, notification_message: "" })
+      enqueueSnackbar(notification_message, {
+        style: {
+          backgroundColor: color_mode === "light" ? "#CCCCCC" : "#333333",
+          color: color_mode === "light" ? "#111111" : "#EEEEEE"
+        }
+      });
+
+      useStore.setState({ notification_alarm: false, notification_message: "" });
     }
-  }, [notification_message, notification_alarm])
+  }, [notification_message, notification_alarm, color_mode])
 
   return null;
 };
 
 const HandleNotifications = () => {
   return (
-    <SnackbarProvider maxSnack={5}>
+    <SnackbarProvider
+      maxSnack={3}
+      autoHideDuration={3000}
+      disableWindowBlurListener={true}
+    >
       <Notify />
     </SnackbarProvider>
   );
