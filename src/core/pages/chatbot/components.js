242f7a3e73a87e8f8479ae7ef32256f6bb6b389c
Ben Chapman <0xbenc@gmail.com>
2024-01-07 02:58:28 -0800

    fixed eslint issues with components

--- src/core/pages/chatbot/components.js
+++ src/core/pages/chatbot/components.js
@@ -7,6 +7,7 @@ import {
   materialDark,
   materialLight,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
+import PropTypes from 'prop-types';
 //
 import { Box } from "@mui/material";
 // ----------------------------------------------------------------------
@@ -16,18 +17,18 @@ const FormattedLeftResponse = ({ content, color_mode }) => {
     <Box sx={{ wordWrap: "break-word", overflowX: "auto", marginY: -1 }}>
       <ReactMarkdown
         rehypePlugins={[rehypeRaw]}
-        children={content}
         components={{
-          code({ node, inline, className, children, ...props }) {
+          code({ inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
               <SyntaxHighlighter
                 {...props}
-                children={String(children).replace(/\n$/, "")}
                 style={color_mode === "light" ? materialLight : materialDark}
                 language={match[1]}
                 PreTag="div"
-              />
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
             ) : (
               <code {...props} className={className}>
                 {children}
@@ -35,7 +36,9 @@ const FormattedLeftResponse = ({ content, color_mode }) => {
             );
           },
         }}
-      />
+      >
+        {content}
+      </ReactMarkdown>
     </Box>
   );
 };
@@ -45,18 +48,18 @@ const FormattedRightResponse = ({ content, color_mode }) => {
     <Box sx={{ wordWrap: "break-word", overflowX: "auto", marginY: -1 }}>
       <ReactMarkdown
         rehypePlugins={[rehypeRaw]}
-        children={content}
         components={{
-          code({ node, inline, className, children, ...props }) {
+          code({ inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return !inline && match ? (
               <SyntaxHighlighter
                 {...props}
-                children={String(children).replace(/\n$/, "")}
                 style={color_mode === "light" ? materialLight : materialDark}
                 language={match[1]}
                 PreTag="div"
-              />
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
             ) : (
               <code {...props} className={className}>
                 {children}
@@ -64,9 +67,21 @@ const FormattedRightResponse = ({ content, color_mode }) => {
             );
           },
         }}
-      />
+      >
+        {content}
+      </ReactMarkdown>
     </Box>
   );
+};
+
+FormattedLeftResponse.propTypes = {
+  content: PropTypes.object.isRequired,
+  color_mode: PropTypes.string.isRequired,
+};
+
+FormattedRightResponse.propTypes = {
+  content: PropTypes.object.isRequired,
+  color_mode: PropTypes.string.isRequired,
 };
 
 export { FormattedLeftResponse, FormattedRightResponse };
